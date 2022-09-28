7b0afb6a5b718b1693eab639f02a8d7536ca00a1
ah-y <sugiura130418@icloud.com>
2022-09-28 11:13:26 +0900

    completed minimal kernel section

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
 //! writing an os by rust
-
 #![no_std] //don't link rust standard library
 #![no_main] //disable all rust-level entry points
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"hell on world, see you dream";
+
+///this `fn` is the entry point, since the linker looks for a `fn` named `_start` by default
 #[no_mangle] //don't mangle the name of this `fn`
 pub extern "C" fn _start() -> ! {
-   //this `fn` is the entry point, since the linker looks for a `fn`
-   //named `_start` by default
+   let vga_buf = 0xb8000 as *mut u8;
+   for (i, &byte,) in HELLO.iter().enumerate() {
+      unsafe {
+         *vga_buf.offset(i as isize * 2,) = byte;
+         *vga_buf.offset(i as isize * 2 + 1,) = 0xb;
+      }
+   }
+
    loop {}
 }
 
