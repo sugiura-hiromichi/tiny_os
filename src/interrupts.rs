438acd5246dbca189ddbed1d635cdd96d01a782b
ah-y <sugiura130418@icloud.com>
2022-10-01 22:48:14 +0900

    complete tests part

---
+++ src/interrupts.rs
@@ -0,0 +1,34 @@
+use {
+   crate::println,
+   lazy_static::lazy_static,
+   x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame},
+};
+
+lazy_static! {
+   static ref IDT: InterruptDescriptorTable = {
+      let mut idt = InterruptDescriptorTable::new();
+      idt.breakpoint.set_handler_fn(breakpoint_handler,);
+      idt.double_fault.set_handler_fn(double_fault_handler,);
+      idt
+   };
+}
+
+extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame,) {
+   println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn double_fault_handler(
+   stack_frame: InterruptStackFrame,
+   _error_code: u64,
+) -> ! {
+   panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+}
+
+///idt stands for InterruptDescriptorTable
+pub fn init_idt() { IDT.load(); }
+
+#[test_case]
+fn test_brewkpoint_exception() {
+   //invoke a breakpoint exception
+   x86_64::instructions::interrupts::int3();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,48 @@
 //! writing an os by rust
 #![no_std] //don't link rust standard library
 #![no_main] //disable all rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(tiny_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use tiny_os::println;
 
-static HELLO: &[u8] = b"hell on world, see you dream";
+static HELLO: &str = "hell on world, see you dream";
 
 ///this `fn` is the entry point, since the linker looks for a `fn` named `_start` by default
 #[no_mangle] //don't mangle the name of this `fn`
 pub extern "C" fn _start() -> ! {
-   let vga_buf = 0xb8000 as *mut u8;
-   for (i, &byte,) in HELLO.iter().enumerate() {
-      unsafe {
-         *vga_buf.offset(i as isize * 2,) = byte;
-         *vga_buf.offset(i as isize * 2 + 1,) = 0xb;
-      }
+   println!("{}", HELLO);
+
+   tiny_os::init();
+   fn stack_overflow() {
+      //for each recursion, the return address is pushed
+      stack_overflow();
    }
 
+   stack_overflow();
+
+   #[cfg(test)]
+   test_main();
+
+   println!("didn't crash!");
    loop {}
 }
 
 ///This `fn` is called on panic
+#[cfg(not(test))] //new attribute
 #[panic_handler]
-fn panic(_info: &PanicInfo,) -> ! { loop {} }
+fn panic(info: &PanicInfo,) -> ! {
+   println!("{}", info);
+   loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo,) -> ! { tiny_os::test_panic_handler(info,) }
+
+#[test_case]
+fn trivial_assertion() {
+   assert_eq!(1, 1);
+}
